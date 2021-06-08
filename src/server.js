fecca3748d3d806c5ab18d655d6bc04151e44259
Kevin Conroy <Kcdrumming@gmail.com>
2021-06-07 20:00:12 -0700

    Added console.log for initial server test

--- src/server.js
+++ src/server.js
@@ -4,20 +4,20 @@ const morganSetting = process.env.NODE_ENV === "production" ? "tiny" : "common";
 app.use(morgan(morganSetting));
 
 app.use((error, req, res, next) => {
-  let response
-  if (process.env.NODE_ENV === 'production') {
-    response = { error: { message: 'server error' }}
+  let response;
+  if (process.env.NODE_ENV === "production") {
+    response = { error: { message: "server error" } };
   } else {
-    response = { error }
+    response = { error };
   }
-  res.status(500).json(response)
-})
+  res.status(500).json(response);
+});
 
-const PORT = process.env.PORT || 8000
-
+const PORT = process.env.PORT || 8000;
 
 app.get("/api/*", (req, res) => {
   res.json({ ok: true });
+  console.log("Success");
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
