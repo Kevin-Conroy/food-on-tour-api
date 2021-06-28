5b11f896877caec98e327b357e4570b680f81320
Kevin Conroy <Kcdrumming@gmail.com>
2021-06-28 16:31:24 -0700

    Updated config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DB_URL || 'postgresql://food_admin@localhost/food_on_tour_api',
+  DATABASE_URL: process.env.DB_URL || 'postgres://saxomykimxeeny:0fd6d418eff2c2107acadd1e80d43696b9db5787e1926c04e0826b10fcc61b71@ec2-35-171-250-21.compute-1.amazonaws.com:5432/dfdsirejbp1mt8',
 }
