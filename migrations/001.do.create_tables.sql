9428744b9745e9a314880659ceafe6c4098f5901
Kevin Conroy <Kcdrumming@gmail.com>
2021-06-29 19:20:03 -0700

    updated migrations

--- migrations/001.do.create_tables.sql
+++ migrations/001.do.create_tables.sql
@@ -30,16 +30,3 @@ CREATE TABLE IF NOT EXISTS bucket_list (
 );
 
 
-INSERT INTO profiles (first_name, last_name, username, bandname, bio, pic_url)
-VALUES ('Kahaleel', 'Nitti', 'user1', 'Automated', 'Front-line multi-state contingency', 'https://www.fillmurray.com/200/300'),
-('Sean', 'Halpeine', 'user2', 'contingency', 'Sharable intangible structure', 'https://www.fillmurray.com/200/301'),
-('Adriano', 'Ferier', 'user3', 'Implemented', 'Synergistic multi-state function', 'https://www.fillmurray.com/200/301' ),
-('Kevin', 'Conroy', 'user4', 'Dave Hause', 'A human being, I am', 'https://www.yelp.com' );
-
-INSERT INTO restaurants (name, city, state, url, price)
-VALUES ('Dynabox', 'Aurora', 'Colorado', 'https://google.com', '$'),
-('Teklist', 'Paterson', 'New Jersey', 'https://yelp.com', '$$'),
-('Oyope', 'Hartford', 'Connecticut', 'https://weather.com', '$$$$');
-
-
-
