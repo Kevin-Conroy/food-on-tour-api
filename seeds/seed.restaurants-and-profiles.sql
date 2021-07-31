d43821ba24d933d3fc70461a5bb96ba813067c00
Kevin Conroy <Kcdrumming@gmail.com>
2021-07-31 07:29:11 -0700

    Added migration for profile password

--- seeds/seed.restaurants-and-profiles.sql
+++ seeds/seed.restaurants-and-profiles.sql
@@ -1,10 +1,9 @@
 BEGIN;
 
 INSERT INTO profiles (first_name, last_name, username, bandname, bio, pic_url)
-VALUES ('Kahaleel', 'Nitti', 'user1', 'Automated', 'Front-line multi-state contingency', 'https://www.fillmurray.com/200/300'),
-('Sean', 'Halpeine', 'user2', 'contingency', 'Sharable intangible structure', 'https://www.fillmurray.com/200/301'),
-('Adriano', 'Ferier', 'user3', 'Implemented', 'Synergistic multi-state function', 'https://www.fillmurray.com/200/301' ),
-('Kevin', 'Conroy', 'user4', 'Dave Hause', 'A human being, I am', 'https://www.yelp.com' );
+VALUES ('Joey', 'Tribiani', 'user1', 'Automated', 'Front-line multi-state contingency', 'https://www.fillmurray.com/200/300'),
+('Sean', 'Halpeine', 'user2', 'Contingency', 'Sharable intangible structure', 'https://www.fillmurray.com/200/301'),
+('Adriano', 'Ferier', 'user3', 'Implemented', 'Synergistic multi-state function', 'https://www.fillmurray.com/200/301' )
 
 INSERT INTO restaurants (name, city, state, url, price)
 VALUES ('Dynabox', 'Aurora', 'Colorado', 'https://google.com', '$'),
